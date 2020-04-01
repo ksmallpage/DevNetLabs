68d8ec70dcfe50e1e94b98f01fd490d3630a65f2
keith smallpage <keith.smallpage@icloud.com>
2020-04-01 14:24:01 -0500

    Updated Comments on TelenetS1VlanConfig.py

--- TelnetS1VlanConfig.py
+++ TelnetS1VlanConfig.py
@@ -2,7 +2,7 @@ import getpass
 import sys
 import telnetlib
 
-HOST = "localhost"
+HOST = "localhost"#add IP address of the switch in place of localhost
 user = raw_input("Enter your Telnet Username: ")
 password = getpass.getpass()
 
@@ -15,15 +15,12 @@ if password:
     tn.write(password + "\n")
 
 tn.write("enable\n")
-tn.write("cisco\n")
+tn.write("cisco\n")#this will be your enable password
 tn.write("conf t\n")
 
-for n in (2, 10):
+for n in (2, 10):#creates Vlan 2 -9
     tn.write("vlan " + str(n) + "\n")
     tn.write("name vlan_" + str(n) + "\n")
 
 tn.write("end\n")
 tn.write("exit\n")
-    
-
-
