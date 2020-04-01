b1d4ca57f086cc8ceb313322d5a2f94b0d446cba
keith smallpage <keith.smallpage@icloud.com>
2020-04-01 14:29:34 -0500

    Updated Comments on TelnetScripts.py

--- TelnetScripts.py
+++ TelnetScripts.py
@@ -2,7 +2,7 @@ import getpass
 import sys
 import telnetlib
 
-HOST = "localhost"
+HOST = "localhost"#enter the IP address of the router management interface
 user = raw_input("Enter your Telnet Username: ")
 password = getpass.getpass()
 
@@ -15,11 +15,10 @@ if password:
     tn.write(password + "\n")
 
 tn.write("enable\n")
-tn.write("cisco\n")
+tn.write("cisco\n")#enter enable password
 tn.write("conf t\n")
-tn.write("int loopback 0\n")
+tn.write("int loopback 0\n")#creates loopback interface
 tn.write("ip add 1.1.1.1 255.255.255.255\n")
 tn.write("end\n")
 tn.write("exit\n")
 tn.write("copy run start\n")
-
